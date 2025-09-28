bf4bf42cd8c78703953b69e315698447a6740a98
Yangxin-Zhang <910858193@qq.com>
2025-09-28 16:18:38 +0800

    revise

--- R/utils-GO_analysis.R
+++ R/utils-GO_analysis.R
@@ -4,18 +4,23 @@
 #' conduct GO enrichment
 #'
 #' @param gene_ls a gene list
+#' @param filtered_genes the filtered genes
 
-.conduct_GO_enrichment <- function(gene_ls) {
+.conduct_GO_enrichment <- function(gene_ls,filtered_genes) {
 
   on.exit(gc())
 
   gene_id <- ICHMousewch::gene_id_information[mgi_symbol %in% gene_ls]
+  universe_id <- ICHMousewch::gene_id_information[mgi_symbol %in% filtered_genes]
 
-  GO_results <- enrichGO(gene = gene_id[],
+  GO_results <- enrichGO(gene = gene_id[,entrezgene_id],
+                         universe = universe_id[,entrezgene_id],
                          OrgDb = org.Mm.eg.db,
                          readable = TRUE,
                          ont = "ALL",
                          pvalueCutoff = 0.05)
+
+  GO_results <- as.data.table(GO_results@results)
 
   return(GO_results)
 
@@ -31,7 +36,7 @@
 
   gene_id <- ICHMousewch::gene_id_information[mgi_symbol %in% gene_ls]
 
-  KEGG_results <- enrichKEGG(gene = gene_id[,],
+  KEGG_results <- enrichKEGG(gene = gene_id[,entrezgene_id],
                              organism = "mmu",
                              keyType = "kegg",
                              pvalueCutoff = 0.05)
@@ -50,7 +55,7 @@
 
   gene_id <- ICHMousewch::gene_id_information[mgi_symbol %in% gene_ls]
 
-  GSEA_results <- gseKEGG(geneList = gene_id[,],
+  GSEA_results <- gseKEGG(geneList = gene_id[,entrezgene_id],
                           organism = "mmu",
                           keyType = "kegg",
                           pvalueCutoff = 0.05)
