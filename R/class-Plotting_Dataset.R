8574af99afeb5cb672ec5cbb37e9acfde40f2727
Yangxin-Zhang <910858193@qq.com>
2025-09-30 09:06:20 +0800

    revise

--- R/class-Plotting_Dataset.R
+++ R/class-Plotting_Dataset.R
@@ -5,11 +5,13 @@
 #'
 #' @slot seurat_metadata_dataset the Seurat Object metadata dataset for plotting
 #' @slot differential_expression_genes_dataset the differential expression genes dataset
+#' @slot GO_enrichment_dataset the dataset of GO enrichment
 #' @slot plots the plots created from dataset
 
 setClass(Class = "Plotting_Dataset",
          slots = c(seurat_metadata_dataset = "data.table",
                    differential_expression_genes_dataset = "data.table",
+                   GO_enrichment_dataset = "data.table",
                    plots = "list"))
 
 #' initialize Plotting_Dataset
@@ -27,6 +29,10 @@ setMethod(f = "initialize",
 
             .Object@differential_expression_genes_dataset <- ich_mouse@diff_expr_genes[[diff_expr_gene_symbol]]
 
+            .Object@GO_enrichment_dataset <- ICHMousewch:::.conduct_GO_enrichment(gene_ls = ich_mouse@diff_expr_genes[["edge-normal"]][avg_log2FC > 1,gene_name],
+                                                                                  filtered_genes = ich_mouse@filtered_genes)
+
+
             validObject(.Object)
             return(.Object)
 
@@ -39,10 +45,12 @@ setMethod(f = "initialize",
 
 Create_Plotting_Dataset <- function(ich_mouse,diff_expr_gene_symbol = "edge-normal") {
 
+  on.exit(gc())
+
   plotting_dataset <- new(Class = "Plotting_Dataset",
                           ich_mouse = ich_mouse,
                           diff_expr_gene_symbol = diff_expr_gene_symbol)
 
-  on.exit(gc())
+  return(plotting_dataset)
 
 }
